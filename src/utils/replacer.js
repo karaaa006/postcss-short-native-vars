0197344764e3be753a75b1beeaf72ac2ecbec80b
Oleh <olegkaraaa@gmail.com>
2022-08-24 14:56:03 +0300

    console log cleared

--- src/utils/replacer.js
+++ src/utils/replacer.js
@@ -3,7 +3,6 @@ export const replacer = (str, isProp) => {
 
 	if (str.match(/\$([A-Za-z0-9_-]*)/g)) {
 		return str.replace(/\$([A-Za-z0-9_-]*)/g, (match, p1) => {
-			console.log(match);
 			if (match[match.length - 1] !== " ") return `var(--${p1})`;
 
 			return `var(--${p1}) `;
